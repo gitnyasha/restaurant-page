4dc7270b7cd1581f5dffeab1c5b75336d23dbc63
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-05-27 18:50:12 +0200

    created page modules

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import { menu, menuPage } from "./modules/menu";
+import { contact, cleanContact } from "./modules/contact";
+import { home, homePage } from "./modules/home";
+
+const tab = () => {
+  const mainDocument = document.querySelector("#content");
+  const navbar = document.createElement("navbar");
+  navbar.id = "navbar";
+
+  const list = document.createElement("ul");
+  list.classList.add("nav-list");
+
+  const menuElements = ["Home", "Menu", "Contacts"];
+
+  for (let el = 0; el < menuElements.length; el++) {
+    let li = document.createElement("li");
+    let anchor = document.createElement("a");
+
+    anchor.innerText = `${menuElements[el]}`;
+    anchor.id = `${menuElements[el]}`;
+    anchor.classList.add("nav-link");
+    anchor.addEventListener("click", () => {
+      getMenuToShow(menuElements[el]);
+    });
+    anchor.href = "#";
+
+    li.appendChild(anchor);
+    list.appendChild(li);
+  }
+  navbar.appendChild(list);
+
+  mainDocument.appendChild(navbar);
+};
+
+function getMenuToShow(element) {
+  cleaner();
+  switch (element) {
+    case "Home":
+      home();
+      break;
+    case "Menu":
+      menu();
+      break;
+    case "Contacts":
+      contact();
+      break;
+    default:
+      break;
+  }
+  return;
+}
+
+const cleaner = () => {
+  cleanContact();
+  menuPage();
+  homePage();
+};
+
+tab();
